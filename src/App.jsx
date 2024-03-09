647f002bef7f3c3a1b4dc4f6a97f40612804d5d2
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-09 12:31:01 +0100

    cards component completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import Analytics from "./component/Analytics";
 import Newsletter from "./component/Newsletter";
+import Cards from "./component/Cards";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Analytics />
       <Newsletter />
+      <Cards />
     </>
   );
 }
