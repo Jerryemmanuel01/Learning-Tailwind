691550868f3dc8ff1a4eaae35fd698945713dfa4
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-09 10:37:37 +0100

    Newslater component completed

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
-  )
-}
+    <div className="w-full py-16 text-white px-4">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my-4">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date.</p>
+        </div>
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+            <input
+              className="p-3 flex w-full rounded-md text-black"
+              type="email"
+              placeholder="Enter Email"
+            />
+            <button className="bg-[#00df9a] w-[200px] rounded-md my-6 ml-4 px-6 py-3 text-black font-medium">
+              Notify Me
+            </button>
+          </div>
+          <p>
+            We care about the protection of your data. read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy</span> 
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}; 
 
-export default Newsletter
+export default Newsletter;
