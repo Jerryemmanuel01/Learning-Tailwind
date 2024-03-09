692dae9e3eb9c23d06c6bcb6ccd6c6d0066ce87e
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-09 10:15:21 +0100

    newsletter component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./component/Navbar";
 import Hero from "./component/Hero";
 import Analytics from "./component/Analytics";
+import Newsletter from "./component/Newsletter";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </>
   );
 }
