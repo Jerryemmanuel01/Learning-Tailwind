e30ed3f52cab439b0245b41abf77edbfe8ebdebc
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-09 12:32:21 +0100

    footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./component/Hero";
 import Analytics from "./component/Analytics";
 import Newsletter from "./component/Newsletter";
 import Cards from "./component/Cards";
+import Footer from "./component/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </>
   );
 }
