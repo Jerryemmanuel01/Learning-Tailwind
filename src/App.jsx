16a3a15d364358b606e8d5ff239e97962a317da6
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-03-09 07:26:05 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
+
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] px-4 mx-auto text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+        REACT TAILWIND
+      </h1>
+      <ul className="flex hidden md:flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resouces</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+      </div>
+
+      <div
+        className={`block md:hidden ${
+          !nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }`}
+      >
+        <h1 className="w-full text-3xl font-bold m-4 text-[#00df9a]">
+          REACT TAILWIND
+        </h1>
+
+        <ul className="uppercase p-4">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resouces</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
+
